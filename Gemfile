f610024ef2bcb1845a2f3a6596ae96960af1a05f
Roberto Lombilla <ironmanbomb@hotmail.com>
2019-08-16 22:17:33 +0000

    all

--- Gemfile
+++ Gemfile
@@ -67,8 +67,11 @@ gem 'simple_form'
 gem "font-awesome-rails"
 gem 'devise'
 gem 'will_paginate', '> 3.0'
+gem "geocoder"
+gem "figaro"
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>=1.3.3'
+
 
 end 
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @places = Place.all
+    @places = Place.order("name").page(params[:page]).per_page(1)
   end
 
 def new
@@ -10,8 +10,13 @@ def new
 end
 
  def create
-    current_user.places.create(place_params)
+  @place = current_user.places.create(place_params)
+  if @place.valid?
     redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
+
   end
 
   def show
@@ -28,12 +33,18 @@ end
 
   def update
      @place = Place.find(params[:id])
+
       if @place.user != current_user
     return render plain: 'Not Allowed', status: :forbidden
   end
     
     @place.update_attributes(place_params)
+     if @place.valid?
     redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end
+  
   end
 
   def destroy
@@ -41,7 +52,7 @@ end
     if @place.user != current_user
     return render plain: 'Not Allowed', status: :forbidden
   end
-  
+
     @place.destroy
     redirect_to root_path
   end
@@ -52,7 +63,6 @@ end
     params.require(:place).permit(:name, :description, :address)
   end
 
-def create
-  end
+
 
 end
