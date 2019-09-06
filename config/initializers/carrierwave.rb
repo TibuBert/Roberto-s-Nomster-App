2d736fa7b2a33767118e6b0f8a9c9f4a1b5f3b46
Roberto Lombilla <ironmanbomb@hotmail.com>
2019-09-06 00:10:27 +0000

    heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,8 @@ CarrierWave.configure do |config|
   config.fog_public = false
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AKIATTA37U2ZC74DMRUT"],        # required
-    aws_secret_access_key: ENV["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"],        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
-  config.fog_directory  = ENV["bertbucketbox"]              # required
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
